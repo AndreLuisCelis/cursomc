63542b7376be19c3b4bc427fa4fcb9ac904ec44f
André Luis Celis <andreluiscelis@celisapp.com>
2019-11-27 21:01:26 -0300

    Testando a primeira classe de dominio -Categoria

--- src/main/java/com/celisapp/cursomc/resource/CategoriaResouce.java
+++ src/main/java/com/celisapp/cursomc/resource/CategoriaResouce.java
@@ -1,15 +1,28 @@
 package com.celisapp.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.celisapp.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResouce {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar () {
-		return "REST esta funcionando";
+	public List<Categoria> listar () {
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
